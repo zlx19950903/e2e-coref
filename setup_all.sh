a8e2ea9658b639bed5c71ffcac90558505dc81eb
guojiantao.elsa@bytedance.com <guojiantao.elsa@bytedance.com>
2021-04-14 09:44:21 +0800

    add coref.so file and modified setup_all.sh python to python3.

--- setup_all.sh
+++ setup_all.sh
@@ -6,8 +6,8 @@ unzip glove.840B.300d.zip
 rm glove.840B.300d.zip
 
 # Build custom kernels.
-TF_CFLAGS=( $(python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_compile_flags()))') )
-TF_LFLAGS=( $(python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_link_flags()))') )
+TF_CFLAGS=( $(python3 -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_compile_flags()))') )
+TF_LFLAGS=( $(python3 -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_link_flags()))') )
 
 # Linux (pip)
 g++ -std=c++11 -shared coref_kernels.cc -o coref_kernels.so -fPIC ${TF_CFLAGS[@]} ${TF_LFLAGS[@]} -O2 -D_GLIBCXX_USE_CXX11_ABI=0
